3e94a0183f8bfca0223789f505e8bcb855b1e5cd
saraardila <saraardiladam@gmail.com>
2023-11-15 20:24:00 +0100

    Navigation Component Done

--- app/src/main/java/com/nawin/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/nawin/horoscapp/ui/home/MainActivity.kt
@@ -3,6 +3,10 @@ package com.nawin.horoscapp.ui.home
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.nawin.horoscapp.R
 import com.nawin.horoscapp.databinding.ActivityMainBinding
 
@@ -11,11 +15,26 @@ class MainActivity : AppCompatActivity() {
     /*Inicializar el binding*/
     private lateinit var  binding:ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         /*Configurar el Binding*/
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment /* convertir a hostFragment
+ */
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
